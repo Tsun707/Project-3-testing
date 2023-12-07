fa6c04ff5e7970477acdeaebf811f68e5ac5a90b
Tsun707 <tsun24@id.iit.edu>
2023-12-06 20:35:06 -0800

    file updating

---
+++ js/script.js
@@ -0,0 +1,9 @@
+  function initMap(){
+  var map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: 43.85756120394474, lng: 18.41114065957382},
+      zoom: 7
+    });
+}
+
+google.maps.event.addDomListener(window, 'load', initMap);
+
